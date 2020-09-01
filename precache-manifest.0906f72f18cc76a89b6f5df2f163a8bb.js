fe4947c0f5813a967a1b35f1f3aa5c51865f2c81
kovgreen <kovgreen@gmail.com>
2020-09-01 11:31:06 +0300

    Updates

---
+++ precache-manifest.0906f72f18cc76a89b6f5df2f163a8bb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4e3023122c964e1b0a86f450dc33ef9",
+    "url": "/goit-react-hw-06-phonebook-redux/index.html"
+  },
+  {
+    "revision": "125aa5b706752d4dbbe1",
+    "url": "/goit-react-hw-06-phonebook-redux/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "eadf301d532d63b4f0ed",
+    "url": "/goit-react-hw-06-phonebook-redux/static/css/main.70454281.chunk.css"
+  },
+  {
+    "revision": "125aa5b706752d4dbbe1",
+    "url": "/goit-react-hw-06-phonebook-redux/static/js/2.81ee0ec7.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/goit-react-hw-06-phonebook-redux/static/js/2.81ee0ec7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eadf301d532d63b4f0ed",
+    "url": "/goit-react-hw-06-phonebook-redux/static/js/main.bb5aea6d.chunk.js"
+  },
+  {
+    "revision": "9c2dca69c59929f6437f",
+    "url": "/goit-react-hw-06-phonebook-redux/static/js/runtime-main.b8cd341e.js"
+  }
+]);
